533e3ca44d4fc763e119f3ba204179418056483d
Ryoung27 <RRYoung89@gmail.com>
2018-09-23 20:18:23 -0500

    Udacity click counter

--- clickCounter/ViewController.swift
+++ clickCounter/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+    }
 
 }
 
